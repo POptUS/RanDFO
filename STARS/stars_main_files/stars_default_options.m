30bdc04b483e10fdf60bc007dde6afc1e2d60929
Stefan M Wild <wild@anl.gov>
2024-08-23 20:17:56 -0700

    misshit

--- STARS/stars_main_files/stars_default_options.m
+++ STARS/stars_main_files/stars_default_options.m
@@ -16,39 +16,39 @@
 %
 % See 'Remarks' below
 
-stars_option.DisplayOutputs = 1;    % 1 to display #iteration, #evaluations, function values and trust-region 
+stars_option.DisplayOutputs = 1;    % 1 to display #iteration, #evaluations, function values and trust-region
                                     % radius during optimization process; 0 otherwise
 stars_option.DisplaySolution = 1;   % 1 to display the best solution at the end of a run; 0 otherwise
 stars_option.EtaOne = 0.01;         % Algorithm parameter $\eta_1$
 stars_option.EtaTwo = 0.9;          % Algorithm parameter $\eta_2$
-stars_option.FiniDiffParam = 0.01;  % Forward finite difference parameter (default value). Updated in the 
+stars_option.FiniDiffParam = 0.01;  % Forward finite difference parameter (default value). Updated in the
                                     % algorithm via the trust-region radius
 stars_option.FixSeed = 0;           % 1 to fix random seeds; 0 otherwise
 stars_option.Gamma = 2;             % Trust-Region parameter gamma
 stars_option.HashingParam = 1;      % s-Hashing parameter: should be less than subspace dimension p
 stars_option.HistoryFile = 1;       % 1 to generate a .txt history file; 0 otherwise (see MCestimate.m)
 stars_option.InitRegionRadius = 1;  % Initial Trust-Region radius
-stars_option.LowerBounds = [];      % Lower bounds on the problem variables; set to -Inf in the algorithm 
+stars_option.LowerBounds = [];      % Lower bounds on the problem variables; set to -Inf in the algorithm
                                     % if no bound is provided
 stars_option.MaxFuncEval = Inf;     % Maximum number of function evaluations (handled by MCestimate.m)
-stars_option.MaxNumberIters = 1000; % Maximum number of iterations (considered only when 
+stars_option.MaxNumberIters = 1000; % Maximum number of iterations (considered only when
                                     % stars_option.MaxFuncEval = Inf)
 stars_option.MaxRegionRadius = 5;   % Maximum Trust-Region radius
 stars_option.SampleSize = 5;        % Sample size for estimate computation (see MCestimate.m)
-stars_option.SeedValue = [];        % Seed value when stars_option.FixSeed = 1; equals floor(1+ pi) in the 
+stars_option.SeedValue = [];        % Seed value when stars_option.FixSeed = 1; equals floor(1+ pi) in the
                                     % algorithm if no value is provided
-stars_option.SolutionFile = 0;      % 1 to generate a .txt file containing the solution of the problem; 
+stars_option.SolutionFile = 0;      % 1 to generate a .txt file containing the solution of the problem;
                                     % 0 otherwise (see [4] below)
 stars_option.StatsFile = 0;         % 1 to generate a .txt stats file; 0 otherwise (see MCestimate.m)
 stars_option.SubspaceDim = 1;       % subspace dimension p (from the Gaussian/Hashing matrices): default value
-stars_option.SubspaceMatrix = 0;    % 0 for Gaussian, 1 for Hashing, 2 for identity matrix (STORM algorithm): 
+stars_option.SubspaceMatrix = 0;    % 0 for Gaussian, 1 for Hashing, 2 for identity matrix (STORM algorithm):
                                     % matrices defining the subspaces
-stars_option.UpperBounds = [];      % Upper bounds on the problem variables; set to +Inf in the algorithm 
+stars_option.UpperBounds = [];      % Upper bounds on the problem variables; set to +Inf in the algorithm
                                     % if no bound is provided
-stars_option.UsePreviousSamples = 1; % 1 to use available samples in cache for estimates computation; 
+stars_option.UsePreviousSamples = 1; % 1 to use available samples in cache for estimates computation;
                                     % 0 otherwise
 stars_option.warning = 0;           % Do not modify
-probspecs.Dimension = [];           % Initialization! Problem dimension set to that of the starting point in 
+probspecs.Dimension = [];           % Initialization! Problem dimension set to that of the starting point in
                                     % the algorithm
 
 %% Remarks

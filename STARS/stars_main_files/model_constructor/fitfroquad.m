4400a8fbdc0902d69b91d4da24348470c7afdd43
Stefan M Wild <wild@lbl.gov>
2024-10-03 09:39:32 -0700

    Update fitfroquad.m

--- STARS/stars_main_files/model_constructor/fitfroquad.m
+++ STARS/stars_main_files/model_constructor/fitfroquad.m
@@ -7,10 +7,10 @@ M = [ones(n + 1, 1) poised_set(1:n + 1, :)]';
 
 if size(poised_set, 1) == n + 1
     H = zeros(n, n);
-    B = M'\f_poised(:)
+    B = M' \ f_poised(:)
     G = B(2:n + 1);
 
-elseif size(poised_set, 1) > n+1
+elseif size(poised_set, 1) > n + 1
     N = zeros(.5 * n * (n + 1), n + 1);
     for np = 1:n + 1
         N(:, np) = phi2eval(poised_set(np, :))';
@@ -34,7 +34,7 @@ elseif size(poised_set, 1) > n+1
         M = [M [1; poised_set(i, :)']]; % Note that M is growing
     end
 
-    % For L=N*Z, solve L'*L*Omega = Z'*f_poised:
+    % For L = N*Z, solve L'*L*Omega = Z'*f_poised:
     Omega = L' \ (Z' * f_poised(:));
     Omega = L \ Omega;
     Beta = L * Omega;
